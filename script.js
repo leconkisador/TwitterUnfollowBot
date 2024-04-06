51fdf8dc5181f74843fe4c5c6617431fbb010924
Conkisador <jeremysterieux@gmail.com>
2024-04-06 05:18:15 +0200

    Fix some bugs (it took me 3 full hours)

--- script.js
+++ script.js
@@ -9,51 +9,47 @@ function Unfollowing(n) {
                 
             } else {
                 console.log(i, "no");
-                Unfollow(i)
+                
+                // Unfollow(i)
+                
             }
         },200*i)       
     }
 }
 
 function UnfollowMax (){
-    const parentElement = document.querySelector("#react-root > div > div > div.css-175oi2r.r-1f2l425.r-13qz1uu.r-417010.r-18u37iz > main > div > div > div > div > div > section > div > div");
 
-    const children = parentElement.children;
+    setTimeout(() => {
+        const parentElement = document.querySelector("#react-root > div > div > div.css-175oi2r.r-1f2l425.r-13qz1uu.r-417010.r-18u37iz > main > div > div > div > div > div > section > div > div");
+
+        const children = parentElement.children;
+        
+
+        for (let i = 0; i < children.length  -1; i++) {
+            
+
+            const child = children[i];
+            const selector = `div:nth-child(${i+1}) > div > div > div > div > div.css-175oi2r.r-1iusvr4.r-16y2uox > div.css-175oi2r.r-1awozwy.r-18u37iz.r-1wtj0ep > div.css-175oi2r.r-1wbh5a2.r-dnmrzs.r-1ny4l3l > div > div.css-175oi2r.r-1awozwy.r-18u37iz.r-1wbh5a2 > div.css-175oi2r.r-1awozwy.r-z2wwpe.r-6koalj.r-1q142lx > div > span`;
+                            // div:nth-child(32) > div > div > div > div > div.css-175oi2r.r-1iusvr4.r-16y2uox > div.css-175oi2r.r-1awozwy.r-18u37iz.r-1wtj0ep > div.css-175oi2r.r-1wbh5a2.r-dnmrzs.r-1ny4l3l > div > div.css-175oi2r.r-1awozwy.r-18u37iz.r-1wbh5a2 > div.css-175oi2r.r-1awozwy.r-z2wwpe.r-6koalj.r-1q142lx ")
+            
+            const element1 = document.querySelector(`#react-root > div > div > div.css-175oi2r.r-1f2l425.r-13qz1uu.r-417010.r-18u37iz > main > div > div > div > div > div > section > div > div > div:nth-child(${i+1}) > div > div > div > div > div.css-175oi2r.r-1iusvr4.r-16y2uox > div.css-175oi2r.r-1awozwy.r-18u37iz.r-1wtj0ep > div.css-175oi2r.r-19u6a5r > div`);
+            const element = child.querySelector(selector);
+                if (!element && !(window.getComputedStyle(element1).getPropertyValue('background-color')==="rgb(15, 20, 25)")) {
+                    
+                    Unfollow(i+1)
+                    UnfollowMax()  
+                    return 
+                    
+                    
+            }        
+                    
+            
+            
+        } 
+    }, 1000);
     
-    let list=[]
-    for (let i = 0; i < children.length  -1; i++) {
-        
-
-        const child = children[i];
-        const selector = `div:nth-child(${i+1}) > div > div > div > div > div.css-175oi2r.r-1iusvr4.r-16y2uox > div.css-175oi2r.r-1awozwy.r-18u37iz.r-1wtj0ep > div.css-175oi2r.r-1wbh5a2.r-dnmrzs.r-1ny4l3l > div > div.css-175oi2r.r-1awozwy.r-18u37iz.r-1wbh5a2 > div.css-175oi2r.r-1awozwy.r-z2wwpe.r-6koalj.r-1q142lx`;
-        const element = child.querySelector(selector);
-        
-        setTimeout(() => {
-            if (element) {
-                
-                list.push("1")
-                
-            } else {
-                
-                Unfollow(i+1) 
-                list.push("0")
-                
-            }
-        }, 300 * (i+1));
-    }
-    const isAllOnes = list.every(item => item === "1");
-
-    if (isAllOnes) {
-        console.log("Fin de l'algo");
-        return  
-    } else {
-        console.log("Le tableau contient au moins un élément différent de '1'.");
-        setTimeout(() => {
-            UnfollowMax()
-        }, 2000);
-        window.location.reload()
-    }
-    console.log(list)
+    
+    
 }
 
 
@@ -67,7 +63,7 @@ function Unfollow(n){
 
     setTimeout(() => {
         document.querySelector("#layers > div:nth-child(2) > div > div > div > div > div > div.css-175oi2r.r-1ny4l3l.r-18u37iz.r-1pi2tsx.r-1777fci.r-1xcajam.r-ipm5af.r-1kihuf0.r-xr3zp9.r-1awozwy.r-1pjcn9w.r-9dcw1g > div.css-175oi2r.r-14lw9ot.r-pm9dpa.r-1rnoaur.r-1867qdf.r-z6ln5t.r-494qqr.r-1jgb5lz.r-13qz1uu > div.css-175oi2r.r-eqz5dr.r-1hc659g.r-1n2ue9f.r-11c0sde.r-13qz1uu > div:nth-child(1)").click()
-    }, 100);
+    }, 200);
     
 }
 
